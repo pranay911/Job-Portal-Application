fca2177f8026997b8e1381a512011b772b6720c5
Pranay Gaikwad <pranaygaikwad1432@gmail.com>
2024-02-26 19:45:13 +0530

    dot-env config added

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 // const express = require("express");
 import express from "express";
+import dotenv from "dotenv";
+
+// Dotenv Config
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Job Portal</h1>");
 });
 
-app.listen(3000, (req, res) => {
-  console.log("App Started on Port:", 3000);
+app.listen(PORT, (req, res) => {
+  console.log(`App Started on Port: ${PORT} in ${process.env.DEV_MODE} mode.`);
 });
