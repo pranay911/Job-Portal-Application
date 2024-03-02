2d623cc21a41a0654ee735c78c56083b285bc490
Pranay Gaikwad <pranaygaikwad1432@gmail.com>
2024-03-02 14:56:32 +0530

    Additional Middleware Added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // const express = require("express");
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
 import connectDB from "./config/db.js";
 import testRoutes from "./routes/testRoutes.js";
 
@@ -11,7 +13,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+// Middlewares
 app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
 // Test route
 app.use("/api/v1/test", testRoutes);
 
